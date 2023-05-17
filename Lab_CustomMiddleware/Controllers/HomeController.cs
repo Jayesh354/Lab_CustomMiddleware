615ceece1b0f205279b377d5d7b9b00c7b45d411
Jayesh Rohit <jayesh.rohit@azilen.com>
2023-05-17 10:13:54 +0530

    Add project files.

---
+++ Lab_CustomMiddleware/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Lab_CustomMiddleware.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab_CustomMiddleware.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Microsoft.AspNetCore.Mvc.Route("/privacy-policy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_CustomMiddleware/Middleware/OldUrlRedirection.cs
@@ -0,0 +1,36 @@
+﻿namespace Lab_CustomMiddleware.Middleware
+{
+    public class OldUrlRedirection
+    {
+        private readonly RequestDelegate _next;
+
+        public OldUrlRedirection(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var url = context.Request.Path.Value;
+            string redirectURL=string.Empty;
+            if (!string.IsNullOrEmpty(url))
+            {
+                url = url.ToLowerInvariant();
+            }
+            switch(url)
+            {
+                case "/home/privacy":
+                    redirectURL = "/privacy-policy";
+                    break;
+                case "/account/login":
+                    redirectURL = "/login";
+                    break;
+            }
+            if(!string.IsNullOrEmpty(redirectURL))
+            {
+                context.Response.Redirect(redirectURL);
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
